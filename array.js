e31a8d8ef9b3b9afa7d13108f16d78eeb4a2d33b
tapader13 <minhajtapader0@gmail.com>
2024-09-04 03:12:07 -0400

    array basic problem solve

--- array.js
+++ array.js
@@ -34,4 +34,41 @@ console.log(fruits.slice(0, 1));
 
 // console.log(Array.from('minhaj'));
 // console.log(Array.from([1, 2, 3], (opt, i) => opt * 2));
-console.log(Array.from({ length: 3 }, (_, i) => i * 2));
+// console.log(Array.from({ length: 3 }, (_, i) => i * 2));
+
+//reverse array
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let ans = [];
+for (const element of arr) {
+  ans.unshift(element);
+}
+console.log(ans);
+
+//sort array
+
+//array problem practice
+//get even numbers
+// let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let index = 0; index < arr1.length; index++) {
+//   if (arr1[index] % 2 !== 0) {
+//     arr1.splice(index, 1);
+//   }
+// }
+// console.log(arr1);
+
+//concat all elements to make 1 string
+// let arr1 = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+// let str = '';
+// for (let index = 0; index < arr1.length; index++) {
+//   str += arr1[index];
+// }
+// console.log(str);
+
+//reverese word of an string
+let arr1 = 'i am minhaj uddin tapader';
+let str = arr1.split(' ');
+let ans1 = [];
+for (const element of str) {
+  ans1.unshift(element);
+}
+console.log(ans1.join(' '));
