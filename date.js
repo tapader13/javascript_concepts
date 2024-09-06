b06a2fa36cbdac689513196420ce5e476301249f
tapader13 <minhajtapader0@gmail.com>
2024-09-06 14:09:37 -0400

    practice some built in math function and basic date methods

---
+++ date.js
@@ -0,0 +1,33 @@
+const date = new Date();
+// console.log(date);
+// console.log(date.getFullYear());
+// console.log(date.getMonth());
+// console.log(date.getDate());
+// console.log(date.getDay());
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getSeconds());
+
+// const specificDate = new Date(2022, 0, 1);
+// console.log(specificDate);
+// const specificDate = new Date('2024-09-06');
+// console.log(specificDate);
+
+// date.setDate(18);
+// date.setMonth(11);
+// date.setFullYear(2024);
+// console.log(date);
+
+// console.log(date.toLocaleDateString('en-GB'));
+// console.log(date.toLocaleDateString('en-US'));
+// console.log(date.toLocaleTimeString());
+// console.log(date.toLocaleString());
+// console.log(date.toDateString());
+// console.log(date.toTimeString());
+
+// const futureDate = new Date();
+// futureDate.setFullYear(futureDate.getFullYear() + 10);
+// console.log(futureDate);
+// const futureDate = new Date();
+// futureDate.setMonth(futureDate.getMonth() - 10);
+// console.log(futureDate);
--- recap.js
+++ recap.js
@@ -56,3 +56,75 @@ let arr = [1, 2, 3, 4, 5];
 
 // let str = 'minukn';
 // console.log(str.indexOf('n', 2));//here it checks from index 2
+
+//convert inches to feet
+// function convertToFeet(inches) {
+//   return `${Math.floor(inches / 12)} ft and ${inches % 12 || 0} inches`;
+// }
+
+// console.log(convertToFeet(73));
+
+//check leap year or not
+// function isLeapYear(year) {
+//   if (year % 4 === 0 && year % 100 !== 0) {
+//     return true;
+//   } else if (year % 400 === 0) {
+//     return true;
+//   }
+//   return false;
+// }
+
+// console.log(isLeapYear(2022));
+
+//some math built in functions
+// console.log(Math.PI);
+// console.log(Math.max(1, 2, 3, 4, 5));
+// console.log(Math.min(1, 2, 3, 4, 5));
+// console.log(Math.pow(2, 3));
+// console.log(Math.sqrt(16));
+// console.log(Math.abs(-10));
+// console.log(Math.floor(10.9));
+// console.log(Math.ceil(10.1));
+// console.log(Math.trunc(10.9)); //remove part after decimal point
+// console.log(Math.round(10.5)); // if above 5 round up, if below 5 round down
+// console.log(Math.floor(Math.random() * 10) + 10); //10 to 20
+// console.log(Math.floor(Math.random() * 10) + 30); //30 to 40
+
+//swap two numbers
+//1
+// let a = 10;
+// let b = 20;
+// a = a + b;
+// b = a - b;
+// a = a - b;
+// console.log(a, b);
+
+//2
+// let a = 10;
+// let b = 20;
+// let t = a;
+// a = b;
+// b = t;
+// console.log(a, b);
+
+//3
+// let a = 10;
+// let b = 20;
+// // [c, d] = [b, a];
+// // console.log(c, d);
+// [a, b] = [b, a];
+// console.log(a, b);
+
+//longest word in string
+// function longestWord(str) {
+//   let arr = str.split(' ');
+//   let longest = arr[0];
+//   for (const element of arr) {
+//     if (element.length > longest.length) {
+//       longest = element;
+//     }
+//   }
+//   return longest;
+// }
+
+// console.log(longestWord('i am minhaj uddin '));
